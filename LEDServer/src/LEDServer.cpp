1b2d5e0e5787331bc5b337dcd761638548449f75
Ashwin Ginoria <ashwinginoria@gmail.com>
2024-06-02 20:12:19 +0100

    -

--- LEDServer/src/LEDServer.cpp
+++ LEDServer/src/LEDServer.cpp
@@ -1,48 +1,160 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <string>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <ctime>
+#include <chrono>
+#include <iostream>
+#include <codecvt>
+
+#include "lwip/udp.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
-#include "boards/pico_w.h"
 
 #include "PicoLed.hpp"
 
-#define LED_PIN 0
+#define LED_PIN 2
 #define LED_LENGTH 288
-const uint8_t MAX_BRIGHTNESS = 50;
-const uint SERVER_PORT = 2020;
+#define MAX_BRIGHTNESS 50
+
+#define PORT_SERVER 5005 
+#define BUFFER_SIZE 1024
+
+#define DEBUG_printf printf
+#define ERROR_printf printf
+
 const char SSID[] = "VM0259584";
 const char PASSWORD[] = "r8qrMmydqxcs";
 
-PicoLed::Color RED = PicoLed::RGB(MAX_BRIGHTNESS, 0, 0);
-PicoLed::Color GREEN = PicoLed::RGB(0, MAX_BRIGHTNESS, 0);
-PicoLed::Color BLUE = PicoLed::RGB(0, 0, MAX_BRIGHTNESS);
+PicoLed::Color GREEN = PicoLed::RGB(255, 0, 0);
+PicoLed::Color RED = PicoLed::RGB(0, 255, 0);
+PicoLed::Color BLUE = PicoLed::RGB(0, 0, 255);
+PicoLed::Color BLACK = PicoLed::RGB(0, 0, 0);
+auto ledStrip = PicoLed::addLeds<PicoLed::WS2812B>(pio0, 0, LED_PIN, LED_LENGTH, PicoLed::FORMAT_RGB);
 
 void debug_break(int);
 
-// Initialize LED
+const char SERVER_IP[] = "0.0.0.0";
 
-// We will run 2 threads 
-//  1. That will control LED lights
-//  2. That checks wifi server for udpates and communicates to 1.
+void process_message(uint8_t* message, int len) {
+    for (int i = 0, offset = 0; i < LED_LENGTH; i++, offset += 3) {
+        ledStrip.setPixelColor(i, PicoLed::RGB(message[offset], message[offset + 1], message[offset + 2]));
+        ledStrip.show();
+    }
+    return;
+}
+
+typedef struct udp_server_t_ {
+    struct udp_pcb *udp;
+    ip_addr_t ip;
+} udp_server_t;
+
+static int udp_socket_new(struct udp_pcb **udp, void *cb_data, udp_recv_fn cb_udp_recv) {
+    *udp = udp_new();
+    if (*udp == NULL) {
+        return -ENOMEM;
+    }
+    udp_recv(*udp, cb_udp_recv, (void *)cb_data);
+    return ERR_OK;
+}
+
+static void udp_socket_free(struct udp_pcb **udp) {
+    if (*udp != NULL) {
+        udp_remove(*udp);
+        *udp = NULL;
+    }
+}
+
+static int udp_socket_bind(struct udp_pcb **udp, uint32_t ip, uint16_t port) {
+    ip_addr_t addr;
+    IP4_ADDR(&addr, ip >> 24 & 0xff, ip >> 16 & 0xff, ip >> 8 & 0xff, ip & 0xff);
+    err_t err = udp_bind(*udp, &addr, port);
+    if (err != ERR_OK) {
+        ERROR_printf("udp failed to bind to port %u: %d", port, err);
+        assert(false);
+    }
+    return err;
+}
+
+static int udp_socket_sendto(struct udp_pcb **udp, const uint8_t *buf, size_t len, const ip_addr_t *dest, uint16_t port) {
+    if (len > 0xffff) {
+        len = 0xffff;
+    }
+
+    struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, len, PBUF_RAM);
+    if (p == NULL) {
+        ERROR_printf("UDP: Failed to send message out of memory\n");
+        return -ENOMEM;
+    }
+
+    memcpy(p->payload, buf, len);
+    err_t err = udp_sendto(*udp, p, dest, port);
+
+    pbuf_free(p);
+
+    if (err != ERR_OK) {
+        ERROR_printf("UDP: Failed to send message %d\n", err);
+        return err;
+    }
+    return len;
+}
+
+static void udp_server_process(void *arg, struct udp_pcb *upcb, struct pbuf *p, const ip_addr_t *src_addr, u16_t src_port) {
+    udp_server_t *d = (udp_server_t*)arg;
+    uint8_t msg[BUFFER_SIZE];
+    size_t msg_len = pbuf_copy_partial(p, msg, sizeof(msg), 0);
+
+    process_message(msg, sizeof(msg));
+}
+
+void udp_server_init(udp_server_t *d, ip_addr_t *ip) {
+    if (udp_socket_new(&d->udp, d, udp_server_process) != ERR_OK) {
+        DEBUG_printf("server failed to start\n");
+        return;
+    }
+    if (udp_socket_bind(&d->udp, 0, PORT_SERVER) != ERR_OK) {
+        DEBUG_printf("server failed to bind\n");
+        return;
+    }
+    ip_addr_copy(d->ip, *ip);
+    DEBUG_printf("server listening on port %d\n", PORT_SERVER);
+}
+
+void udp_server_deinit(udp_server_t *d) {
+    udp_socket_free(&d->udp);
+}
+
+void udp_server() {
+    ip_addr_t addr;
+    IP4_ADDR(ip_2_ip4(&addr), 0, 0, 0, 0);
+
+    udp_server_t _server;
+    udp_server_init(&_server, &addr);
+
+    int count_requests = 0;
+
+    // 10 ms sleep => 100 fps
+    while(true) {
+        sleep_ms(10);
+    }
+
+    udp_server_deinit(&_server);
+}
 
 int connect_wifi() {
     return cyw43_arch_wifi_connect_async(SSID, PASSWORD, CYW43_AUTH_WPA_TKIP_PSK);
 }
 
-int run()
-{
+int main() {
     stdio_init_all();
-
     // Set Max Brightness for LEDs
-    auto ledStrip = PicoLed::addLeds<PicoLed::WS2812B>(pio0, 0, LED_PIN, LED_LENGTH, PicoLed::FORMAT_RGB);
     ledStrip.setBrightness(MAX_BRIGHTNESS);
 
     // Run
     ledStrip.fill(BLUE);
     ledStrip.show();
-
-    debug_break(30);
 
     // Initialize Network Driver
     if (cyw43_arch_init()) {
@@ -54,40 +166,22 @@ int run()
     cyw43_arch_enable_sta_mode();
 
     // Connect to Wifi
-    printf("Connecting to Wi-Fi...\n");
+    DEBUG_printf("Connecting to Wi-Fi...\n");
     if (cyw43_arch_wifi_connect_timeout_ms(SSID, PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
         printf("failed to Connect\n");
+        ledStrip.fill(RED);
+        ledStrip.show();
         return 1;
     } else {
-        printf("Connected.\n");
+        DEBUG_printf("Connected.\n");
+        ledStrip.fill(GREEN);
+        ledStrip.show();
     }
-    
-    // Start LED Lights Server in a new thread
-    // Continue Wifi Server in main thread here.
 
-    while (true) {
-        int status = cyw43_tcpip_link_status(&cyw43_state, CYW43_ITF_STA);
-
-        printf("status : %d, comparison: %d\n", status, status == 3);
-        if (status == 3) {
-            printf("GREEN\n", status);
-            ledStrip.fill(GREEN);
-            ledStrip.show();
-        }
-
-        cyw43_arch_poll();
-        sleep_ms(500);
-    }
+    DEBUG_printf("Starting UDP Server\n");
+    udp_server();
+    DEBUG_printf("Closing UDP Server\n");
 
     cyw43_arch_deinit();
-}
-
-void debug_break(int _seconds) {
-    printf("Taking a Break!!");
-    sleep_ms(_seconds * 100);
-    printf("Running");
-}
-
-int main() {
-    return run();
+    return 0;
 }
--- client.py
+++ client.py
@@ -1,20 +1,21 @@
 import socket
 import time
 import json
+from typing import List
 
 SERVER_IP = "192.168.0.244"
 UDP_PORT = 5005
 TCP_PORT = 80
-LED_LENGTH = 100
+LED_LENGTH = 288
 
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
-WHITE = (255, 255, 255)
+BLACK = [0, 0, 0]
+RED = [255, 0, 0]
+YELLOW = [255, 150, 0]
+GREEN = [0, 255, 0]
+CYAN = [0, 255, 255]
+BLUE = [0, 0, 255]
+PURPLE = [180, 0, 255]
+WHITE = [255, 255, 255]
 COLORS = (BLACK, RED, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE)
 
 udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -23,36 +24,36 @@ tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def send_udp(cmd: bytes) -> None:
     udp_sock.sendto(cmd, (SERVER_IP, UDP_PORT))
 
-def tcp_connect():
-    tcp_sock.connect((SERVER_IP, TCP_PORT))
-
-def send_tcp(cmd: bytes) -> None:
-    tcp_sock.send(cmd)
-
 def send_cmd(cmd: bytes) -> None:
-    print("Sending ", len(cmd))
-    send_tcp(cmd)
+    send_udp(cmd)
 
-tcp_connect()
-
-send_cmd(b"Hello World")
+# tcp_connect()
 
 def set_color(color):
     leds = [color] * LED_LENGTH
-    send_cmd(json.dumps(leds).encode('utf-8'))
+    send_cmd(convert_bytes(leds))
+
+def convert_bytes(pixels: List[List[int]]) -> List[bytes]:
+    flat_list = []
+    for pixel in pixels:
+        for led in pixel:
+            flat_list.append(led)
+    return bytearray(flat_list)
 
 def test_blink():
     on = True
     while True:
         try: 
             if (on):
-                set_color(WHITE)
+                set_color(GREEN)
+                print("ON")
                 on = False
             else:
+                print("OFF")
                 set_color(BLACK)
                 on = True
             
-            time.sleep(0.05)
+            time.sleep(0.10)
         except KeyboardInterrupt as e:
             print("End")
             break
@@ -60,3 +61,11 @@ def test_blink():
     set_color(BLACK)
     
 test_blink()
+
+# set_color(RED)
+
+def tcp_connect():
+    tcp_sock.connect((SERVER_IP, TCP_PORT))
+
+def send_tcp(cmd: bytes) -> None:
+    tcp_sock.send(cmd)
