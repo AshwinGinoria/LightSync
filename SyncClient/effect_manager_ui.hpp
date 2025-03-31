ef571e8c57443f1e21eeb3c5ac327e8dbe6385b1
Ashwin Ginoria <ashwinginoria@gmail.com>
2025-03-31 03:06:11 +0100

    feat: process color picker input

--- SyncClient/effect_manager_ui.hpp
+++ SyncClient/effect_manager_ui.hpp
@@ -1,24 +1,24 @@
 #pragma once
 
-#include <QMainWindow>
+#include "parameter.hpp"
 #include <QComboBox>
 #include <QFormLayout>
+#include <QMainWindow>
 #include <map>
 #include <string>
-#include "parameter.hpp"
 
 class EffectManagerUI : public QMainWindow {
     Q_OBJECT
 
-public:
+  public:
     explicit EffectManagerUI(QWidget *parent = nullptr);
 
-private slots:
+  private slots:
     void loadEffectParameters(const QString &effectName);
     void startEffect();
     void stopEffect();
 
-private:
+  private:
     QComboBox *effectList;
     QFormLayout *parameterForm;
     std::map<std::string, QWidget *> inputWidgets;
